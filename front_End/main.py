3d9c8e24e91165fc79f17cc4bd6a8fecf3bd7403
devsda12 <jmbalster@hotmail.com>
2018-12-17 18:08:31 +0100

    Several Changes

--- front_End/main.py
+++ front_End/main.py
@@ -1,8 +1,10 @@
+import front_End.log as log_Module
 import pyttsx3
 import speech_recognition as sr
+import datetime
 
 
-#Initialization function for the speech output
+##Initialization function for the speech output##
 def initializeSpeech():
     primary_engine = pyttsx3.init()
 
@@ -13,18 +15,20 @@ def initializeSpeech():
     return primary_engine
 
 
-#Initialization function for the speech listener
+##Initialization function for the speech listener##
 def initializeListener():
     r = sr.Recognizer()
+
+    with sr.Microphone(device_index=4) as source:
+        r.adjust_for_ambient_noise(source)
 
     return r
 
 
-#Ambient listener for the keyword
+##Ambient listener for the keyword##
 def ambientListener():
     #Starting with catching audio from microphone
     with sr.Microphone(device_index=4) as source:
-        listener.adjust_for_ambient_noise(source)
         print("Listener online! Talk!")
         audio = listener.listen(source)
 
@@ -39,7 +43,7 @@ def ambientListener():
     return data
 
 
-#Speak function to output the given string to speech
+##Speak function to output the given string to speech##
 def speak(speakString):
     if isinstance(speakString, str):
         primary_engine.say(speakString)
@@ -49,11 +53,19 @@ def speak(speakString):
         primary_engine.runAndWait()
 
 
-#Initial calls under here
+##Initial calls under here##
+
+#Text-to-speech setup
 primary_engine = initializeSpeech()
+
+#Speech-to-text setup
 listener = initializeListener()
 
-#Test calls under here
+#Log setup
+logCreateDate = datetime.datetime.now()
+log_Module.logSetup(logCreateDate)
+
+##Test calls under here##
 while True:
     data = ambientListener()
     speak(data)
--- mainDir/main.py
+++ mainDir/main.py
@@ -1,2 +1,23 @@
 import mainDir.log as log_Module
+import flask
+from flask import request
+import datetime
 
+##Flask setupcalls under here##
+app = flask.Flask(__name__)
+
+##Flask defenitions##
+@app.route('/testDef', methods=['GET'])
+def testDef():
+    i = "The test is working"
+    log_Module.logModule("API GET request return: " + i, logCreateDate, datetime.datetime.now())
+    return "The test is working"
+
+##Initial calls under here##
+
+#Log setup
+logCreateDate = datetime.datetime.now()
+log_Module.logSetup(logCreateDate)
+
+#Starting the API
+app.run()
